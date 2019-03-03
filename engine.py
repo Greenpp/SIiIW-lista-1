df515860896236a769f0f40304a010e32694df4c
Jakub Ciszek <jakubciszek64@gmail.com>
2019-03-03 21:33:25 +0100

    Initial population, refactorization

--- engine.py
+++ engine.py
@@ -1,20 +1,26 @@
-from entity import Node, Item
+from entity import Node, Item, Entity
 
 
 class Engine:
     DATA_DIR = 'data/'
 
-    def __init__(self):
+    def __init__(self, population_size):
         self.problem_name = None
         self.knapsack_data_type = None
-        self.node_num = None
-        self.item_num = None
+        self.nodes_num = None
+        self.items_num = None
         self.max_capacity = None
         self.min_speed = None
         self.max_speed = None
         self.renting_ratio = None
         self.edge_weight_type = None
         self.nodes = []
+
+        self.population_size = population_size
+        self.population = []
+
+    def init(self):
+        self.population = [Entity(self.nodes_num) for i in range(self.population_size)]
 
     def load_data(self, file_name):
         """
@@ -29,16 +35,16 @@ class Engine:
 
         self.problem_name = lines[0].split(':')[1].replace('\t', '').replace('\n', '')
         self.knapsack_data_type = lines[1].split(':')[1].replace('\t', '').replace('\n', '')
-        self.node_num = int(lines[2].split(':')[1].replace('\t', '').replace('\n', ''))
-        self.item_num = int(lines[3].split(':')[1].replace('\t', '').replace('\n', ''))
+        self.nodes_num = int(lines[2].split(':')[1].replace('\t', '').replace('\n', ''))
+        self.items_num = int(lines[3].split(':')[1].replace('\t', '').replace('\n', ''))
         self.max_capacity = int(lines[4].split(':')[1].replace('\t', '').replace('\n', ''))
         self.min_speed = float(lines[5].split(':')[1].replace('\t', '').replace('\n', ''))
         self.max_speed = float(lines[6].split(':')[1].replace('\t', '').replace('\n', ''))
         self.renting_ratio = float(lines[7].split(':')[1].replace('\t', '').replace('\n', ''))
         self.edge_weight_type = lines[8].split(':')[1].replace('\t', '').replace('\n', '')
 
-        node_lines = lines[10:self.node_num + 10]
-        item_lines = lines[self.node_num + 11:self.node_num + self.item_num + 11]
+        node_lines = lines[10:self.nodes_num + 10]
+        item_lines = lines[self.nodes_num + 11:self.nodes_num + self.items_num + 11]
 
         for node_line in node_lines:
             _, x, y = node_line.split()
