e814ea4b89838974c83a5f2689ee1080b2d69cf8
Jakub Ciszek <jakubciszek64@gmail.com>
2019-03-06 19:37:53 +0100

    Simple mutations

--- genetics.py
+++ genetics.py
@@ -27,3 +27,25 @@ class Genotype:
         phenotype.append((self.nodes_order[-1], self.nodes_order[0]))
 
         return phenotype
+
+    def mutation_swap(self):
+        """
+        Mutation swaps two random nodes
+        """
+        # positions to swap
+        pos1, pos2 = random.sample(range(len(self.nodes_order)), 2)
+
+        tmp = self.nodes_order[pos1]
+        self.nodes_order[pos1] = self.nodes_order[pos2]
+        self.nodes_order[pos2] = tmp
+
+    def mutation_inverse(self):
+        """
+        Mutation inverses genotype fragment
+        """
+        # beginning and end of fragment to inverse
+        pos1, pos2 = sorted(random.sample(range(len(self.nodes_order)), 2))
+
+        # inverse <pos1, pos2>
+        for i, v in zip(range(pos2 - pos1 + 1), reversed(self.nodes_order[pos1:pos2 + 1])):
+            self.nodes_order[pos1 + i] = v
