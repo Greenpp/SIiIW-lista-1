bfbdd9ae420d31aa7f37097359980b6c839518fc
Jakub Ciszek <jakubciszek64@gmail.com>
2019-03-06 21:23:12 +0100

    Full algorithm test

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from engine import Engine
 
-eng = Engine(10)
+eng = Engine(100)
 
-eng.load_data('trivial_1.ttp')
-eng.init()
-
+eng.load_data('hard_1.ttp')
 eng.greedy_item_select()
 
-eng.test()
-eng.sort()
+eng.init()
+
+for i in range(1000):
+    print('Best fitness: {}'.format(eng.population[0].fitness))
+    eng.next_generation()
