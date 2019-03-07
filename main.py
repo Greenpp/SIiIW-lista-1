369aaefb4a9323a74778dc0e8d1e1dfb36f21854
Jakub Ciszek <jakubciszek64@gmail.com>
2019-03-07 20:51:50 +0100

    Parameterized engine test

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from engine import Engine
 
-eng = Engine(100)
-
+eng = Engine()
 eng.load_data('easy_1.ttp')
-eng.greedy_item_select(method='ratio')
 
-eng.init()
-
-for i in range(1000):
-    print('Best fitness: {}'.format(eng.population[0].fitness))
-    eng.next_generation()
-
-eng.plot_data()
+eng.run(generations=1000, info_every=10)
